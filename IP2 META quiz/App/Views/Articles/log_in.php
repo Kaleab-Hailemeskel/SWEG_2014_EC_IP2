d7484f813fc76cff634a3142b38246a87fa7c51c
Kaleab Hailemeskel <126022543+Kaleab-Hailemeskel@users.noreply.github.com>
2024-05-10 12:05:50 +0300

    Add files via upload
    2 php files for login and signin. the other 2 are helper files

---
+++ IP2 META quiz/App/Views/Articles/log_in.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include("clean_input.php");
+include("dbcon.php");
+
+
+$user_email = $user_password = "";
+if(isset($_POST["login_btn"])){
+    $user_email = clean_input( $_POST["email"]);
+    $user_password = clean_input($_POST["pass"]);
+
+    $search_query = "SELECT * FROM DB WHERE email = '$user_email' AND password = '$user_password' LIMIT 1";
+    $search_query_run =  mysqli_query($con, $search_query);
+
+    if(mysqli_num_rows($search_query_run) == 1){
+        $_SESSION["curr"] = "correct password login allowed";
+        echo "correct email and password<br>";
+    }
+
+}
+
+
+
+
+
+?>
---
+++ IP2 META quiz/App/Views/Articles/sign_up.php
@@ -0,0 +1,67 @@
+<?php
+// REGISTRATION PHP FILE
+session_start();
+include("clean_input.php");
+$user_email = $user_password = $user_cof_password = $user_school_level = $user_full_name = "";
+$can_register = true;
+
+if($_REQUEST["REQUEST_METHOD"] !== "POST"){
+    echo "invalid request method<br>";
+    
+}
+else{
+    $user_email = clean_input($_POST["email"]);
+    $user_password = clean_input($_POST["password"]);
+    $user_cof_password = clean_input($_POST["passwordconfirm"]);
+    $user_school_level = $_POST["school-level"];
+    $user_full_name = clean_input($_POST["name"]);
+
+    if(!preg_match("/^[a-zA-Z-' ]*$/", $user_full_name)){
+        echo "Only letters and spaces are allowed <br>";
+        return;
+    }
+    if(!filter_var($user_email , FILTER_VALIDATE_EMAIL)){
+        echo "invalid email format<br>";
+        return;
+    }
+    if(!preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/", $user_password)){
+        echo ("Password must contain both small and capital characters including underscore and numbers<br>");
+        return;
+    }
+    if($user_cof_password !== $user_password){
+        echo " two different passwords found in the form<br>";
+        return;
+    }
+    
+    include("dbcon.php");
+    $search_query = "SELECT * FROM DB WHERE email = '$user_email' LIMIT 1";
+    $search_query_run =  mysqli_query($con, $search_query);
+
+    if(mysqli_num_rows($search_query_run) > 0 ){
+        $_SESSION['curr'] = "user already exist";
+        echo "user already exist<br>";
+        header("Location: sigh_up.html");
+        return;
+    }
+    else{
+        $register_query = "INSERT INTO DB(username, password,email,role) VALUES ('$user_name', '$user_password'. '$user_email', 'user')";
+        $register_query_run = mysqli_query($con, $register_query);
+
+        if($register_query_run){
+            $_SESSION['curr'] = "registeration successfull";
+            echo "Successful Registration<br>";
+        }
+        else{
+            $_SESSION['curr'] = "registration went wrong";
+            echo "registration went wrong<br>";
+        }
+
+    }
+
+    
+}
+
+
+
+
+?>
