5c8cde73974fd29287f243c3d962c174c9f8e999
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-05-18 09:24:07 +0300

    Update log_in.php

--- IP2 META quiz/App/Views/Articles/log_in.php
+++ IP2 META quiz/App/Views/Articles/log_in.php
@@ -9,7 +9,7 @@ if(isset($_POST["login_btn"])){
     $user_email = clean_input( $_POST["email"]);
     $user_password = clean_input($_POST["pass"]);
 
-    $search_query = "SELECT * FROM DB WHERE email = '$user_email' AND password = '$user_password' LIMIT 1";
+    $search_query = "SELECT * FROM quiz_db WHERE email = '$user_email' AND password = '$user_password' LIMIT 1";
     $search_query_run =  mysqli_query($con, $search_query);
 
     if(mysqli_num_rows($search_query_run) == 1){
