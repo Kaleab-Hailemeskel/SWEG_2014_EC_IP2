93bc38ff2561d361d058ab4122d8066a6d3dde39
Kaleab <kaleabmureja123@gmail.com>
2024-05-26 14:35:12 +0900

    Builting database connection with MongoDB

--- Udemy_Project/api/app.js
+++ Udemy_Project/api/app.js
@@ -1,13 +1,23 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
+
+const connectDB = require('./server/config/db.js');
+
+var indexRouter = require('./server/routes/index');
+var usersRouter = require('./server/routes/users');
+var testAPIRouter = require('./server/routes/testAPI');
+
 var app = express();
+connectDB();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,8 +25,17 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'secret_key',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -38,7 +57,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
 
 module.exports = app;
