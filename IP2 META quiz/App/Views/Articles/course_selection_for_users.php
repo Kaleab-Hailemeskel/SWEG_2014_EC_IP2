870b72a3dd517befd828a7a685b8e3027709677a
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-06-13 19:02:24 +0300

    Update course_selection_for_users.php

--- IP2 META quiz/App/Views/Articles/course_selection_for_users.php
+++ IP2 META quiz/App/Views/Articles/course_selection_for_users.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <html>
 <head> <link rel="stylesheet" href="../Resources/css/hfStyle.css">
 <link rel="stylesheet" href="../Resources/css/course_selection.css"> 
@@ -6,15 +9,16 @@
 <img id="Logo" src="../Resources/Img/MQ.png" alt="Meta Quiz Logo"/>
 <div class="listContainer">
 <ul id="headerList"> 
-<li id="headerListPoints" class="rightBorder"> <a id="home" href="index.html" style="color:white;"> Home </a> </li> 
-<li id="headerListPoints" class="rightBorder"><a id="home" href="course_selection.html" style="color:white;"> Practice </a> </li>
+<li id="headerListPoints" class="rightBorder"> <a id="home" href="index.php" style="color:white;"> Home </a> </li> 
+<li id="headerListPoints" class="rightBorder"><a id="home" href="course_selection.php" style="color:white;"> Practice </a> </li>
 <li id="headerListPoints"><a id="home" href="about_us.html" style="color:white;"> About Us </a> </li>
 </ul>
+
 </div>
 
 <div id="LogTab">
-<span id="LogIn"><a id="home" href="log_in.html" style="color:white;">Log In</a> </span>
-<span id="SignUp"> <a id="home" href="sign_up.html"style="color:white;"> Sign Up </a></span>
+<span id="LogIn"><a id="home" href="log_in.php" style="color:white;">Log In</a> </span>
+<span id="SignUp"> <a id="home" href="sign_up.php" style="color:white;"> Sign Up </a></span>
 <div>
 </header>
 <body>
@@ -42,6 +46,7 @@ $password="";
 $dbname="quiz_db";
 $connection=mysqli_connect($servername,$username,$password,$dbname);
     $user_id =$_SESSION['user_id'];
+    echo $user_id;
     $query="SELECT quiz_id,quiz_name,user_id from user_quizzes where user_id='$user_id'";
 
 $result=mysqli_query($connection,$query);
@@ -62,13 +67,95 @@ mysqli_close($connection);
         <div class="button-container">
             <a id="courseLinks" href="course_present_user.php?quiz_id=<?php echo urlencode($quiz['quiz_id']);?>&quiz_name=<?php echo urlencode($quiz['quiz_name'])?>">
                 <button class = "btn-course"><?php echo $quiz['quiz_name']?></button> </a>
-    
+            <a href="#" class="delete-quiz" data-quiz-id="<?php echo urlencode($quiz['quiz_id']);?>" data-user-id="<?php echo urlencode($user_id);?>">
+            <button class="btn-delete">Delete</button>
+            </a>
         </div>
     </div>
 
 <?php endforeach;?>
 </tr>
+<tr>
+
+<div >
+<a href="add_quiz.php">
+
+    <button class="btn-adds">Add Quizzes</button>
+</a>
+</div>
+</tr>
+<tr>
+<div >
+<a href="stat_choice.php">
+    <button class="btn-stat">See Stats</button>
+</a>
+</div>
+
+</div>
+</tr>
+<style>
+  .btn-stat, .btn-adds {
+  background-color: pink; 
+  color: black; 
+  border: none; 
+  padding: 10px 20px; 
+  text-align: center; 
+  text-decoration: none;
+  display: inline-block; 
+  font-size: 16px;
+  margin: 4px 2px; 
+  cursor: pointer; 
+  border-radius: 8px; 
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); 
+  transition: 0.3s; 
+}
+
+.btn-adds {
+  float: left; 
+}
+
+.btn-stat {
+  float: right; 
+}
+
+.btn-adds:hover, .btn-stat:hover {
+  box-shadow: 0 12px 16px 0 rgba(255, 192, 203, 0.8), 0 17px 50px 0 rgba(255, 192, 203, 0.19); /* Glow effect on hover */
+}
+  
+</style>
+    
 </table>
+<script  src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" >
+  
+</script>
+<script>
+$(document).ready(function(){
+    $('.delete-quiz').click(function(e){
+        e.preventDefault();
+        console.log("dlete button clicked");
+        var quiz_id = $(this).data('quiz-id');
+        var user_id=$(this).data('user-id')
+        
+        $.ajax({
+            url: 'delete_quiz.php',
+            type: 'POST',
+            data: {
+                'quiz_id': quiz_id,
+                'user_id':user_id
+            },
+            success: function(response){
+                console.log("ajax request succeful",response);
+                if(response == 'success'){
+                    $('div[data-quiz-id="' + quiz_id + '"]').remove();
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+        console.log('AJAX request failed', textStatus, errorThrown);
+    } 
+        }); console.log("ajx reuest sent");
+    });
+});
+</script>
 </body>
 
 
